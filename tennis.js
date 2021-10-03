f39b8b0bd13e7da07bc50aceb7fae641179d5066
Harpreetattri2020 <harpreetattri2020@gmail.com>
2021-10-04 00:11:06 +0100

    Logic Building and doing testing

--- tennis.js
+++ tennis.js
@@ -18,5 +18,63 @@ class Tennis{
        3:"forty",
 
    };
+   score(){
+
+       if(this.isScoreDifference()){
+
+           if(this.isReadyForGamePoint()){
+               if(this.isAdv()){
+                   
+                return this.advScore();
+               }
+               return this.winScore();
+
+           }
+         return this.lookupScore();
+       }
+
+       if(this.isDeuce()){
+         return this.Deuce();
+       }
+       return this.sameScore();
+       
+   }
+   winScore(){
+       return `${this.advPlayer()} Win`;
+   }
+   isScoreDifference(){
+       return this.secondPlayerScoreTimes !==this.firstPlayerScoreTimes;
+   }
+   isReadyForGamePoint(){
+       return this.firstPlayerScoreTimes>3 ||this.secondPlayerScoreTimes>3;
+
+   }
+
+   isAdv(){
+       return Math.abs(this.firstPlayerScoreTimes-this.secondPlayerScoreTimes)===1;
+   }
+   sameScore(){
+       return `${this.scoreLookup[this.firstPlayerScoreTimes]} all`;
+   }
+   Deuce(){
+       return "deuce";
+   }
+   
+   isDeuce(){
+       return this.firstPlayerScoreTimes>=3; 
+   }
+
+   lookupScore(){
+       return `${this.scoreLookup[this.firstPlayerScoreTimes]} ${this.scoreLookup[this.secondPlayerScoreTimes]}`;
+   }
+
+   advScore(){
+       return `${this.advPlayer()} Adv`;
+   }
+
+   
+   advPlayer(){
+       return this.firstPlayerScoreTimes > this.secondPlayerScoreTimes ? this.firstPlayerName : this.secondPlayerName;
+   }
 }
 module.exports=Tennis;
--- tennis.test.js
+++ tennis.test.js
@@ -4,5 +4,72 @@ let tennis;
 describe("Tennis Game", () => {
   beforeEach(() => {
     tennis = new Tennis("Harpreet","Miki");
-  })
-})
+  });
+  test("love all", () => {
+    scoreShouldBe("love all");
+  });
+  test("fifteen love", () => {
+    tennis.firstPlayerScore(1);
+    scoreShouldBe("fifteen love");
+  });
+  test("thirty love", () => {
+    tennis.firstPlayerScore(2);
+    scoreShouldBe("thirty love");
+  });
+  test("forty love", () => {
+    tennis.firstPlayerScore(3);
+    scoreShouldBe("forty love");
+  });
+  test("love fifteen", () => {
+    tennis.secondPlayerScore(1);
+    scoreShouldBe("love fifteen");
+  });
+  test("love thirty", () => {
+    tennis.secondPlayerScore(2);
+    scoreShouldBe("love thirty");
+  });
+  test("fifteen all", () => {
+    tennis.secondPlayerScore(1);
+    tennis.firstPlayerScore(1);
+    scoreShouldBe("fifteen all");
+  });
+  test("thirty all", () => {
+    tennis.secondPlayerScore(2);
+    tennis.firstPlayerScore(2);
+    scoreShouldBe("thirty all");
+  });
+  test("forty thirty", () => {
+    tennis.secondPlayerScore(2);
+    tennis.firstPlayerScore(3);
+    scoreShouldBe("forty thirty");
+  });
+  test("forty fifteen", () => {
+    tennis.secondPlayerScore(1);
+    tennis.firstPlayerScore(4);
+    scoreShouldBe("forty fifteen");
+  });
+  test("deuce", () => {
+    tennis.secondPlayerScore(3)
+    tennis.firstPlayerScore(3)
+    scoreShouldBe("deuce");
+  });
+   test("Harpreet Adv", () => {
+     tennis.firstPlayerScore(4);
+     tennis.secondPlayerScore(3);
+     scoreShouldBe("Harpreet Adv");
+   });
+  test("Miki Adv", () => {
+    tennis.firstPlayerScore(3);
+    tennis.secondPlayerScore(4);
+    scoreShouldBe("Miki Adv");
+  });
+  test("Miki Win", () => {
+    tennis.firstPlayerScore(2)
+    tennis.secondPlayerScore(4);
+    scoreShouldBe("Miki Win");
+  });
+});
+
+function scoreShouldBe(expected) {
+  expect(tennis.score()).toBe(expected);
+}
